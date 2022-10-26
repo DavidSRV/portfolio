a1df1b24007f8d8bbe6569fccca162c95ee661c6
David Ruiz <david.ruizvargas@hotmail.com>
2022-10-26 00:17:59 -0500

    Se inicia portfolio

---
+++ src/components/mainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainPage() {
+  return (
+    <section>
+        <h1>Hi, I'm David</h1>
+        <p>Dev Fronted</p>
+    </section>
+  )
+}
+
+export default MainPage
