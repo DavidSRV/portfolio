61171f6102db4f8d8c15f2050236afffe9757cd4
David Ruiz <david.ruizvargas@hotmail.com>
2022-10-30 23:49:50 -0500

    Se mejoran CV y Skills

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -5,18 +5,25 @@ import "./SkillsStyle.scss";
 function Skills() {
   return (
     <>
-      <h3>Experiencia</h3>
-      <p>He estado haciendo desarrollo web durante 1 año, y siempre estoy ansioso por aprender nuevas tecnologías y mejorar.</p>
-      <section className="skills__General">
-        <p>Tecnologias y herramientas que he usado  </p>
-        <article className="skills__container">
-          {skills.map((element) => (
-            <div className={element.name} key={element.name}>
-              <p className="skills__title">{element.name}</p>
-              {element.img}
-            </div>
-          ))}
-        </article>
+      <h3 className="title__skills">Experiencia</h3>
+      <p className="description__skills">
+        He estado desarrollando apliaciones y paginas web durante 1 año. <br />
+        Siempre estoy ansioso por aprender nuevas tecnologías y mejorar.
+      </p>
+      <section className="skills__Exp">
+        <aside className="skills__General">
+          <p className="skills__description">
+            Tecnologias y herramientas que he usado{" "}
+          </p>
+          <article className="skills__container">
+            {skills.map((element) => (
+              <div className={element.name} key={element.name}>
+                <p className="skills__title">{element.name}</p>
+                {element.img}
+              </div>
+            ))}
+          </article>
+        </aside>
       </section>
     </>
   );
