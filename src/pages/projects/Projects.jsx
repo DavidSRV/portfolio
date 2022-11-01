d3394e73a0e3a7f2fab37d7ff2647dbfa157360f
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-01 02:10:04 -0500

    Se hace sketch de projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import { projects } from "../../utils/projects";
+import "./_ProjectsStyle.scss";
 
 function Projects() {
   return (
-    <section className='portfolio'>
-      <p className='portfolio__title'>PORTAFOLIO DE PROYECTOS</p>
-      <article className='portflio__projects'>
-
-      </article>
+    <section className="portfolio">
+      <p className="portfolio__title">PORTAFOLIO DE PROYECTOS</p>
+      <p className="portfolio__subtitle">(Más proyectos muy pronto...)</p>
+      <article className="portfolio__projects" >
+      {projects.map((item) => (
+          <img key={item.name} className={item.name} src={item.img} alt={item.name} />
+          ))}
+          </article>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
---
+++ src/utils/projects.js
@@ -0,0 +1,45 @@
+import TODO from "../assets/img/TODO.jpg"
+import Ecommerce from "../assets/img/E-Commerce.jpg"
+import CreditCard from "../assets/img/CrediCard.jpg"
+import TrackerDash from "../assets/img/TimeTrackerDash.jpg"
+import Testimonial from "../assets/img/Testimonials.jpg"
+import AdiviceAPI from "../assets/img/AdviceAPI.jpg"
+
+export const projects = [
+    {
+        name: "TodoManagment",
+        urlRepo: "https://github.com/DavidSRV/TodoManagment",
+        urlLive: "https://davidsrv.github.io/TodoManagment/",
+        img: TODO,
+    },
+    {
+        name: "E-commerce",
+        urlRepo: "https://github.com/DavidSRV/E-commerce",
+        urlLive: "https://davidsrv.github.io/E-commerce/",
+        img: Ecommerce,
+    },
+    {
+        name: "CreditCard",
+        urlRepo: "https://github.com/DavidSRV/CreditCard",
+        urlLive: "https://davidsrv.github.io/CreditCard/",
+        img: CreditCard,
+    },
+    {
+        name: "TimeTrackerDash",
+        urlRepo: "https://github.com/DavidSRV/TimeTrackerDash",
+        urlLive: "https://davidsrv.github.io/TimeTrackerDash/",
+        img: TrackerDash,
+    },
+    {
+        name: "TestimonialsGrid",
+        urlRepo: "https://github.com/DavidSRV/TestimonialsGridSection",
+        urlLive: "https://davidsrv.github.io/TestimonialsGridSection/",
+        img: Testimonial,
+    },
+    {
+        name: "AdviceAPI",
+        urlRepo: "https://github.com/DavidSRV/AdviceGenerator",
+        urlLive: "https://davidsrv.github.io/AdviceGenerator/",
+        img: AdiviceAPI,
+    },
+]
