dadae20edf5319ebcf7372450e05caf34d01ef0c
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-03 01:58:28 -0500

    Se empieza maquetación footer

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -8,10 +8,26 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__containerIcons">
-        <BsGithub className="footer__icon --git" />
-        <BsLinkedin className="footer__icon --lin" />
-        <BsWhatsapp className="footer__icon --wha" />
-        <BsInstagram className="footer__icon --inst" />
+        <a href="https://github.com/DavidSRV" target="_blank" rel="noreferrer">
+          <BsGithub className="footer__icon --git" />
+        </a>
+        <a href="https://walink.co/e875fa" target="_blank" rel="noreferrer">
+          <BsWhatsapp className="footer__icon --wha" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/david-sebasti%C3%A1n-ruiz-vargas-05b197223/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin className="footer__icon --lin" />
+        </a>
+        <a
+          href="https://www.instagram.com/davidruizvargas/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram className="footer__icon --inst" />
+        </a>
       </div>
       <div className="footer__up">
         <IoRocketOutline />
