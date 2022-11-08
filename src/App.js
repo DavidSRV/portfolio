c143af8454f94b7e3fcc544383632050a0bd70b4
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-07 23:45:26 -0500

    Se termina proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import MainPage from "./pages/main/MainPage";
 import "./style/style.scss";
+import { Routes, Route } from 'react-router-dom'
+import MainPage from "./pages/main/MainPage";
+
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <Routes>
+      <Route path="/portfolio" element={<MainPage />} />
+    </Routes>
     </div>
   );
 }
