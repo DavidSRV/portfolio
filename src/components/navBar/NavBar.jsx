efa9ff65cf2dde44cf0ceb4ecd9f8f7bacf73afe
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-07 22:19:12 -0500

    Se termina portfolio

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useRef } from "react";
 import "./_NavBarStyle.scss";
 
 function NavBar() {
+  const navBar = useRef(null);
+
+  let scrollUbication = window.pageYOffset;
+
+  window.onscroll = () => {
+    let move = window.pageYOffset;
+    if (scrollUbication >= 828) {
+      if (scrollUbication >= move) {
+        navBar.current.style.display = "none";
+        navBar.current.style.trasition = "All 1s";
+
+      } else {
+        navBar.current.style.display = "flex";
+      }
+    }
+    scrollUbication = move;
+  };
+
   return (
-    <nav className="links__containers">
+    <nav ref={navBar} className={`links__containers`}>
       <div className="links__general">
         <a href="#about">SOBRE MÍ</a>
         <a href="#skills">TECNOLOGÍAS</a>
